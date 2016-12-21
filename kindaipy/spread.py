dbd49cb6e29420ba1ce03b59b9982fa65c699041
Eiji Warashina <eiji.warashina@gmail.com>
2016-12-21 14:04:50 +0900

    Lint spread.py by flake8.

--- kindaipy/spread.py
+++ kindaipy/spread.py
@@ -1,6 +1,7 @@
 import kindaipy.util as util
 from collections import OrderedDict
 
+
 class Spread():
     def __init__(self, book, spread_number):
         self.book = book
@@ -8,8 +9,9 @@ class Spread():
 
     def image_url(self) -> str:
         params = OrderedDict([
-            ('itemId'     ,'info:ndljp/pid/{0}'.format(str(self.book.key))),
-            ('contentNo'  , str(self.spread_number)),
+            ('itemId',      'info:ndljp/pid/{0}'.format(str(self.book.key))),
+            ('contentNo',   str(self.spread_number)),
             ('outputScale', '1'),
         ])
-        return 'http://dl.ndl.go.jp/view/jpegOutput?{0}'.format(util.expand_params(params))
+        return 'http://dl.ndl.go.jp/view/jpegOutput?{0}'.format(
+          util.expand_params(params))
