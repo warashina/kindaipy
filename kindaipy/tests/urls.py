02565f8c5691efd15dec34b61b977cdbc9bfe504
Eiji Warashina <eiji.warashina@gmail.com>
2016-12-24 17:10:23 +0900

    Add tests/urls.py. This is test data for to create Book object. support_url will acceptable by kindaipy nonsupport_url will not acceptable by kindaipy, because they have no jpeg file to download.
    テストデータ用のurlリストを作成した。
    国会デジタルライブラリーの資料は項目があったりなかったり名前が違ったりするので、
    メタデータ作成時に資料ごとにエラーが出たりする可能性がある。
    面白そうだと思った資料はsupport_urlsに加えておくし、エラーが出た資料も修正対応でき次第ここに加える。
    
    JPEGファイルがダウンロードできない資料もnonsupport_urlsとしてメモしておく。
    サポート外エラー表示のためにとっておく。

---
+++ kindaipy/tests/urls.py
@@ -0,0 +1,23 @@
+support_urlrs = [
+'http://kindai.ndl.go.jp/info:ndljp/pid/922693', # 正義熱血社出版部 正義の叫
+# 言語
+'http://dl.ndl.go.jp/info:ndljp/pid/2938229',  # 山田美妙 日本大辞書 全 六版
+'http://dl.ndl.go.jp/info:ndljp/pid/862876',   # 落合直文 ことばの泉
+'http://dl.ndl.go.jp/info:ndljp/pid/863017',   # 金沢庄三郎 辞林
+'http://dl.ndl.go.jp/info:ndljp/pid/992954',   # 大槻文彦 編　言海 : 日本辞書. 第1-4冊
+'http://dl.ndl.go.jp/info:ndljp/pid/1265336',  # 大槻文彦 著 大言海. 第3巻
+'http://dl.ndl.go.jp/info:ndljp/pid/1114203',  # 平凡社編 大辞典 volume: 第23巻
+'http://dl.ndl.go.jp/info:ndljp/pid/992501',   #　物集高見 纂　日本大辞林
+'http://dl.ndl.go.jp/info:ndljp/pid/1873360',  # 平凡社編 大辞典 第七卷
+# 民俗
+'http://dl.ndl.go.jp/info:ndljp/pid/969095',   # 物集高見 著 広文庫. 第1冊
+'http://dl.ndl.go.jp/info:ndljp/pid/969114',   # 物集高見 著 広文庫. 広文庫. 第20冊
+# War
+'http://dl.ndl.go.jp/info:ndljp/pid/1718962',  # 久米元一 著[他] 西住戦車長 : 昭和の軍神
+]
+
+nonsupport_urls = [
+# No jpeg spreads
+'http://dl.ndl.go.jp/info:ndljp/pid/6006431', # pdf document
+'http://dl.ndl.go.jp/info:ndljp/pid/2964146', # Index page of Kanpo
+]
